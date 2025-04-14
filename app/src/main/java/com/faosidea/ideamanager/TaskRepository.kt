313140fb724091706a1a5e4ce43aad45a182a895
Henry Letting <lettinghenry0@gmail.com>
2025-04-14 13:11:30 +0300

    added TaskRepository to interface with the database through Dao

---
+++ app/src/main/java/com/faosidea/ideamanager/TaskRepository.kt
@@ -0,0 +1,24 @@
+package com.faosidea.ideamanager
+
+import androidx.lifecycle.LiveData
+
+class TaskRepository (private val taskDao: TaskDao) {
+
+    val allTasks: LiveData<List<Task>> = taskDao.getAllTasks()
+
+    suspend fun insert(task: Task) {
+        taskDao.insertTask(task)
+    }
+
+    suspend fun update(task: Task) {
+        taskDao.updateTask(task)
+    }
+
+    suspend fun delete(task: Task) {
+        taskDao.deleteTask(task)
+    }
+
+    suspend fun getTaskById(taskId: Long) : Task? {
+        return taskDao.getTaskById(taskId)
+    }
+}
